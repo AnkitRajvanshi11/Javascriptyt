3f9a43c2aca0227a8ada0e4e3b34f951aa3213cd
AnkitRajvanshi11 <ankurajvanshi16@gmail.com>
2024-09-23 11:32:35 +0530

    javascript

---
+++ basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer  vision
+
+//alert(3 + 3) we are using nodejs ,  not browser
+
+console.log(3 + 3);
+
+console.log("ankit")
+
+
+let name ="ankit"
+let age = 21
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigint
+// String=> ""
+// boolean => false/true
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+//object
+
+console.log(typeof undefined);  //undefiend
+console.log(typeof null);  //object
+
---
+++ basic/variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "ankit@gmail.com"
+var accountPassword ="12345"
+accountCity = "Ballia"
+let accountstate;
+
+// nodeaccountId = 2
+
+/*
+    prefer not to use var 
+    because of issue in block scope and functional scope
+*/
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "09870"
+accountCity = "agra"
+
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountstate]);
+
+
+
